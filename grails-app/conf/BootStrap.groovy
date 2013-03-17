0097a31233aeb0bf62f095f54aa24f8e5a691549
suryazi <suryazi@abg.com.sa>
2013-03-17 13:55:15 +0300

    Shiro implemented

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,26 +1,32 @@
 import com.alworoud.Role
 import com.alworoud.User
-import org.apache.shiro.crypto.hash.sha512Hash
+import org.apache.shiro.crypto.hash.Sha512Hash
 
 class BootStrap {
-
+   
+    def shiroSecurityService
+   
     def init = { servletContext ->
+   
         def adminRole = new Role(name: "Administrator")
         adminRole.addToPermissions("*:*")
         adminRole.save()
-        
-        def userRole = new Role(name: "User")
-        userRole.addToPersmissions("Owner:*")
+       
+        def userRole = new Role(name:"User")
+        userRole.addToPermissions("Owner:*")
         userRole.save()
-        
-        def admin = new User(username: "Admin", passwordHash: new sha512Hash("password").toHex())
+       
+        def admin = new User(username: "Admin", passwordHash: new Sha512Hash("password").toHex())
         admin.addToRoles(adminRole)
         admin.save()
-        
-        def user = new User(username: "User", passwordHash: new sha512Hash("pass").toHex())
+       
+        def user = new User(username: "User", passwordHash: new Sha512Hash("password").toHex())
         user.addToRoles(userRole)
         user.save()
+              
     }
+
+   
     def destroy = {
     }
 }
